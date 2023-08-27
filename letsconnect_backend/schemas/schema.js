23c5eb24ba7c47cbe96b2d293c83e6f1f3577cf3
Zishnendu Sarker <94640101+zishnusarker@users.noreply.github.com>
2023-08-27 17:26:00 +0600

    aa

--- letsconnect_backend/schemas/schema.js
+++ letsconnect_backend/schemas/schema.js
@@ -1,9 +1,14 @@
-//First, ipmorting schema creator is a must 
-import createSchema from 'part:@sanity/base/schema-crator'
+// //First, ipmorting schema creator is a must 
+// import createSchema from 'part:@sanity/base/schema-crator'
 
-//Then import schema types frim any plugins that might expose them 
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+// //Then import schema types frim any plugins that might expose them 
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+
 import user from './user';
+import pin from './pin';
+import comments from './comments';
+import postedBy from './postedBy';
+import save from './save';
 
 //then we give our schema to the builder and provise the result to schema
 export default createSchema({
@@ -13,8 +18,7 @@ export default createSchema({
     types: schemaTypes.concat([
         /*the types are here*/
 
-        user
+        user, pin, comments, postedBy, save
 
     ])
 })
-
