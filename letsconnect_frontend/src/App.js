1131cd704d34ec175511181cf7298ad381ee57d4
Zishnendu Sarker <94640101+zishnusarker@users.noreply.github.com>
2023-08-29 11:03:17 +0600

    aa

--- letsconnect_frontend/src/App.js
+++ letsconnect_frontend/src/App.js
@@ -7,13 +7,18 @@
 // }
 
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './container/Home';
+
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello Connections
-        <p>this is my first react project</p>
-    </h1>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   )
 }
 
